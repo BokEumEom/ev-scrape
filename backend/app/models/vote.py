d476b3ec3aa7aeed249630e5b02d5cfd5fba5a04
bokeum <bokeum.eom@bepsolar.com>
2024-03-07 15:50:14 +0900

    backend 프로젝트 삭제

--- backend/app/models/vote.py
+++
@@ -1,9 +0,0 @@
-# app/models/vote.py
-from sqlalchemy import Column, Integer, String
-from app.db.database import Base
-
-class Vote(Base):
-    __tablename__ = "votes"
-    
-    id = Column(Integer, primary_key=True, index=True)
-    vote_type = Column(String, nullable=False)
