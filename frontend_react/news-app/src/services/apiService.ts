5ae685faa3eba8da613e7873bb5de9a539c9997d
bokeum <bokeum.eom@bepsolar.com>
2024-03-07 16:10:42 +0900

    api endpoint 변경

--- frontend_react/news-app/src/services/apiService.ts
+++ frontend_react/news-app/src/services/apiService.ts
@@ -1,7 +1,7 @@
 // src/services/apiService.ts
 export const fetchNewsItems = async (page = 1, limit = 10) => {
     const skip = (page - 1) * limit; // Calculate the correct skip value
-    const url = `http://localhost:8000/news?skip=${skip}&limit=${limit}`;
+    const url = `https://fastapi.watercharging.com/news?skip=${skip}&limit=${limit}`;
     try {
         const response = await fetch(url);
         if (!response.ok) {
