69d41e90f36b6cc9a3c89d3c9a7611577ed22b07
bokeum <bokeum.eom@bepsolar.com>
2024-03-07 16:24:39 +0900

    swipe 기능 추가

--- frontend_react/news-app/src/pages/NewsPage.tsx
+++ frontend_react/news-app/src/pages/NewsPage.tsx
@@ -1,13 +1,13 @@
-// src/pages/NewsPage.tsx
 import React, { useEffect, useState } from 'react';
-import { NewsItem } from '../types'; // Adjust if necessary
+import { useSwipeable } from 'react-swipeable';
+import { NewsItem } from '../types';
 import { fetchNewsItems } from '../services/apiService';
 import NewsList from '../components/NewsList';
 
 const NewsPage: React.FC = () => {
     const [newsItems, setNewsItems] = useState<NewsItem[]>([]);
     const [page, setPage] = useState(1);
-    const limit = 10; // Adjust based on your preference or backend setup
+    const limit = 10;
 
     useEffect(() => {
         fetchNewsItems(page, limit)
@@ -18,16 +18,21 @@ const NewsPage: React.FC = () => {
         window.scrollTo(0, 0);
     }, [page]);
 
+    const handlers = useSwipeable({
+        onSwipedLeft: () => setPage(page + 1), // 오른쪽에서 왼쪽으로 스와이프 시 다음 페이지
+        onSwipedRight: () => setPage(page > 1 ? page - 1 : 1), // 왼쪽에서 오른쪽으로 스와이프 시 이전 페이지
+        trackMouse: true, // 마우스로도 스와이프 가능
+    });
+
     return (
-        <div className="px-4">
+        <div {...handlers}>
             <NewsList newsItems={newsItems} />
             <div className="flex justify-center mt-4">
-                <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-3 md:px-4 rounded-lg shadow mx-2 disabled:opacity-50" onClick={() => setPage(page > 1 ? page - 1 : 1)} disabled={page === 1}>Previous</button>
-                <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-3 md:px-4 rounded-lg shadow mx-2" onClick={() => setPage(page + 1)}>Next</button>
+                <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg shadow mx-2 disabled:opacity-50" onClick={() => setPage(page > 1 ? page - 1 : 1)} disabled={page === 1}>Previous</button>
+                <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg shadow mx-2" onClick={() => setPage(page + 1)}>Next</button>
             </div>
         </div>
     );
 };
 
 export default NewsPage;
-
